111d66dd7a213ebac8336afedaa9b0f2f32c4d97
domorsolutions <momaurice24@gmail.com>
2025-05-20 22:03:57 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Simple service worker for Animal Art Adventure app
+const CACHE_NAME = 'animal-art-adventure-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install and cache core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
